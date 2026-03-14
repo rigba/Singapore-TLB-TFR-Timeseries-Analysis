46ec9408c1dd32b381b13a2bc2728e3d838140ca
godmode1111 <71916438+rigba@users.noreply.github.com>
2026-03-14 15:17:00 +1030

    Dual axis graph and analysis

--- script.R
+++ script.R
@@ -38,3 +38,32 @@ tfr <- data_long %>%
   select(Year, TFR = Value) %>%
   as_tsibble(index = Year)
 
+
+# Now I wanna create a dual axis graph of TLB and TFR over time so I need a scaling factor
+# I need to put the data in year, tfr value, tlb value first
+
+data_wide <- left_join(tlb, tfr, by = "Year")
+
+
+scale_factor <- max(data_wide$TLB, na.rm = TRUE) / max(data_wide$TFR, na.rm = TRUE)
+
+ggplot(data_wide, aes(x = Year)) +
+  geom_line(aes(y = TLB, colour = "TLB"), linewidth = 1) +
+  geom_line(aes(y = TFR * scale_factor, colour = "TFR"),
+            linewidth = 1) +
+  scale_y_continuous(name = "Total Live Births",
+                     sec.axis = sec_axis( ~ . / scale_factor, name = "Total Fertility Rate")) +
+  scale_colour_manual(name = NULL,
+                      values = c("TLB" = "red", "TFR" = "blue")) +
+  labs(x = "Year") +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.title.y = element_text(color = "red"),
+    axis.title.y.right = element_text(color = "blue")
+  )
+
+
+# The presence trend is easy to spot via visual inspection of this dual axis graph
+# Since the data is not monthly or quarterly we are unlikely to spot any seasonal trends
+
